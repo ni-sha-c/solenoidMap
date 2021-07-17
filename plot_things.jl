720e30bac3eed1a346d566082252a0f53c6135f3
Nisha Chandramoorthy <nishac@mit.edu>
2021-07-17 15:00:13 -0400

    read data from S3

--- plot_things.jl
+++ plot_things.jl
@@ -1,5 +1,6 @@
 using PyPlot
 using DelimitedFiles
+using JLD
 X01 = readdlm("data/dJds2_0.01.txt")
 X05 = readdlm("data/dJds2_0.05.txt")
 nS01 = X01[:,1]
@@ -7,6 +8,14 @@ nS05 = X05[:,1]
 dJds01 = X01[:,2]
 dJds05 = X05[:,2]
 truth = dJds05[1]
+
+#s3 parameter in S3 is actually s2 in the Fd code.
+X = load("data/dJds3_K12_N.jld")
+nSS3 = X["n_samples"]
+dJdsS3 = X["dJds"]
+
+
+
 fig, ax = subplots()
 ax.loglog(nS01, abs.((dJds01.-truth)./truth), ".-", ms=20)
 ax.loglog(nS05, abs.((dJds05.-truth)./truth), "P-", ms=20)
